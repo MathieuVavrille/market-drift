cc40d484cde039d6118454a0c0ddbfff26a3b82b
Mathieu Vavrille (Swing) <mathieu.vavrille@gmail.com>
2025-04-14 23:11:56 +0200

    cart movement improved, need to do the reverse penalty

--- player_cart.gd
+++ player_cart.gd
@@ -1,20 +1,33 @@
 extends RigidBody2D
 
-@export var move_force := 500.0
+@export var move_force := 1000.0
 @export var max_speed := 200.0
 
-func _ready():
-	linear_damp = 1.0    # adjust to taste: higher = more drag
-	angular_damp = 1.0   # reduce cart spinning
+var FRICTION_STRENGTH = move_force / max_speed * 10
 
 func _physics_process(delta):
+	var cart_direction = $CartMesh.global_position - $PlayerMesh.global_position
 	var input_vector = Vector2(
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
 		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	)
-	print(Input.get_action_strength("ui_right"))
-	print(input_vector)
+	).normalized()
 
+	apply_central_force(-linear_velocity * FRICTION_STRENGTH)
+	apply_torque(-angular_velocity * 20)
 	if input_vector.length() > 0:
 		var current_velocity = linear_velocity
-		apply_force(input_vector * move_force, $PlayerCollision.global_position - global_position)
+		var force_position = $PlayerMesh.global_position - global_position
+		var angle = cart_direction.angle_to(input_vector)
+		if angle < PI:
+			angle += PI
+		if angle > PI:
+			angle -= PI
+		if abs(angle) < PI/16:
+			apply_torque(angle * 10000)
+		if abs(angle) < PI/2:
+			apply_force(input_vector * move_force, force_position)
+			print(angle)
+		else:  # Does not work well ...
+			print("here")
+			apply_force(input_vector * move_force / 4, force_position)
+		
