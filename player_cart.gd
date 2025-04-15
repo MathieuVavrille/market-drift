f386d583379460c9c61be09e675af5725fe1b038
Mathieu Vavrille (Swing) <mathieu.vavrille@gmail.com>
2025-04-15 23:18:51 +0200

    starting tiles

--- player_cart.gd
+++ player_cart.gd
@@ -7,27 +7,29 @@ var FRICTION_STRENGTH = move_force / max_speed * 10
 
 func _physics_process(delta):
 	var cart_direction = $CartMesh.global_position - $PlayerMesh.global_position
+	print(cart_direction)
 	var input_vector = Vector2(
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
 		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	).normalized()
-
+	
+	# Drag
 	apply_central_force(-linear_velocity * FRICTION_STRENGTH)
 	apply_torque(-angular_velocity * 20)
+	
 	if input_vector.length() > 0:
 		var current_velocity = linear_velocity
 		var force_position = $PlayerMesh.global_position - global_position
 		var angle = cart_direction.angle_to(input_vector)
-		if angle < PI:
+		if angle < -PI:
 			angle += PI
 		if angle > PI:
 			angle -= PI
+		print(angle * 180 / PI)
 		if abs(angle) < PI/16:
 			apply_torque(angle * 10000)
 		if abs(angle) < PI/2:
 			apply_force(input_vector * move_force, force_position)
-			print(angle)
-		else:  # Does not work well ...
-			print("here")
+		else:
 			apply_force(input_vector * move_force / 4, force_position)
 		
