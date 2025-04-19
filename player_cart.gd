647506f81544f65e18afdbb3fa2681ad98a2a35b
Mathieu Vavrille (Swing) <mathieu.vavrille@gmail.com>
2025-04-19 19:12:43 +0200

    drawn sprites

--- player_cart.gd
+++ player_cart.gd
@@ -6,8 +6,7 @@ extends RigidBody2D
 var FRICTION_STRENGTH = move_force / max_speed * 10
 
 func _physics_process(delta):
-	var cart_direction = $CartMesh.global_position - $PlayerMesh.global_position
-	print(cart_direction)
+	var cart_direction = ($CartMesh.global_position - $PlayerMesh.global_position).normalized()
 	var input_vector = Vector2(
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
 		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -18,18 +17,27 @@ func _physics_process(delta):
 	apply_torque(-angular_velocity * 20)
 	
 	if input_vector.length() > 0:
-		var current_velocity = linear_velocity
-		var force_position = $PlayerMesh.global_position - global_position
-		var angle = cart_direction.angle_to(input_vector)
-		if angle < -PI:
-			angle += PI
-		if angle > PI:
-			angle -= PI
-		print(angle * 180 / PI)
-		if abs(angle) < PI/16:
-			apply_torque(angle * 10000)
-		if abs(angle) < PI/2:
-			apply_force(input_vector * move_force, force_position)
+		if Input.is_action_pressed("turn"):
+			var proj = (input_vector.dot(cart_direction) / cart_direction.length_squared()) * cart_direction
+			var orth = input_vector - proj
+			var cross = input_vector.cross(cart_direction)
+			print(cross)
+			apply_torque(-cross * 2500)
+			var force_position = $PlayerMesh.global_position - global_position
+			apply_force(proj * move_force, force_position)
 		else:
-			apply_force(input_vector * move_force / 4, force_position)
+			var current_velocity = linear_velocity
+			var force_position = $PlayerMesh.global_position - global_position
+			var angle = cart_direction.angle_to(input_vector)
+			if angle < -PI:
+				angle += PI
+			if angle > PI:
+				angle -= PI
+			print(angle * 180 / PI)
+			if abs(angle) < PI/16:
+				apply_torque(angle * 10000)
+			if abs(angle) < PI/2:
+				apply_force(input_vector * move_force, force_position)
+			else:
+				apply_force(input_vector * move_force / 1, force_position)
 		
