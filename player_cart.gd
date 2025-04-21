7856de12adcb18040933ac9427ab32e5cd1b7f8a
Mathieu Vavrille (Swing) <mathieu.vavrille@gmail.com>
2025-04-21 22:46:54 +0200

    starting object area

--- player_cart.gd
+++ player_cart.gd
@@ -5,7 +5,7 @@ extends RigidBody2D
 
 var FRICTION_STRENGTH = move_force / max_speed * 10
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var cart_direction = ($CartMesh.global_position - $PlayerMesh.global_position).normalized()
 	var input_vector = Vector2(
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
@@ -16,12 +16,11 @@ func _physics_process(delta):
 	apply_central_force(-linear_velocity * FRICTION_STRENGTH)
 	apply_torque(-angular_velocity * 20)
 	
-	if input_vector.length() > 0:
+	if input_vector != Vector2.ZERO:
 		if Input.is_action_pressed("turn"):
 			var proj = (input_vector.dot(cart_direction) / cart_direction.length_squared()) * cart_direction
 			var orth = input_vector - proj
 			var cross = input_vector.cross(cart_direction)
-			print(cross)
 			apply_torque(-cross * 2500)
 			var force_position = $PlayerMesh.global_position - global_position
 			apply_force(proj * move_force, force_position)
@@ -33,11 +32,37 @@ func _physics_process(delta):
 				angle += PI
 			if angle > PI:
 				angle -= PI
-			print(angle * 180 / PI)
 			if abs(angle) < PI/16:
 				apply_torque(angle * 10000)
 			if abs(angle) < PI/2:
 				apply_force(input_vector * move_force, force_position)
 			else:
 				apply_force(input_vector * move_force / 1, force_position)
-		
+	else: # no movement
+		if inside_object != null:
+			apply_central_force(-linear_velocity * 100)
+			apply_torque(-angular_velocity * 1000)
+			if linear_velocity.length() < 0.25 and abs(angular_velocity) < 0.1:
+				linear_velocity = Vector2.ZERO
+				angular_velocity = 0
+
+func is_stopped():
+	return linear_velocity == Vector2.ZERO and angular_velocity == 0
+
+func _process(delta: float) -> void:
+	if inside_object != null and is_stopped():
+		$ProgressBar.visible = true
+		$ProgressBar.value += delta * 50
+		if $ProgressBar.value >= 100:
+			$ProgressBar.value =0.
+			$ProgressBar.visible = false
+			inside_object = null
+	else:
+		$ProgressBar.value =0.
+		$ProgressBar.visible = false
+
+var inside_object = null
+func entered_object(area: Area2D) -> void:
+	inside_object = area
+func object_exited(_area: Area2D) -> void:
+	inside_object = null
